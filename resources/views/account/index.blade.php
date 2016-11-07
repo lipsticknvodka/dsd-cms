c07b5f67dc134d3d719958c62a53f260d8439570
Nick Boyle <nsboyle@gmail.com>
2016-11-07 10:06:13 -0500

    Modify view to be similar to survey.index

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,2 +1,9 @@
-<h1>Account Index</h1>
+<h1>Welcome to Account Creation</h1>
 
+@include('errors.list')
+
+{!! Form::open() !!}
+{!! Form::label('account_no', 'account_no') !!}<br>
+{!! Form::text('account_no') !!}<br>
+<button type="submit">Submit</button>
+{!! Form::close() !!}
