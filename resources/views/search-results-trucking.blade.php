df26ea31dc9c273493c89a7f8f3b3f45c710ecf6
Michelle Prather <michelleprather@Michelles-MacBook-Pro.local>
2017-03-21 18:02:52 -0700

    Freight Availability customer search layout fixed

--- resources/views/search-results-trucking.blade.php
+++ resources/views/search-results-trucking.blade.php
@@ -13,7 +13,7 @@
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-xs-12 col-sm-10">
+                <div class="col-xs-12 col-sm-10 col-sm-offset-1">
                     <div class="panel panel-default">
                 <div class="panel panel-default">
                     <div class="panel-heading">Trucking Delivery Details</div>
