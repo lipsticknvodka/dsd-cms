854a8bb30df446db252db5eb5cc344aafbe0b13f
Michelle Prather <michelleprather@Michelles-MacBook-Pro.local>
2017-05-14 11:01:22 -0700

    Mailgun credentials added to config/services

--- config/services.php
+++ config/services.php
@@ -15,8 +15,8 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain' => env('www.dsdcompanies.com'),
+        'secret' => env('key-8c7417ff5e579233cf27dc6899ba2437'),
     ],
 
     'ses' => [
